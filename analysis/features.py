fa9f703ea8044923e59f9cf551b2126fe12b699a
Ricardo Hortelano <ricardo.hortelano@intelygenz.com>
2019-03-13 22:17:24 +0100

    initial commit, added faced_ratios

---
+++ analysis/features.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def faced_ratios(dataframe, colums, class_column, n_rows=2, n_cols=3, plot_area=True, legend=True, colors=None, figure=None):
+    if not colors:
+        colors = ['blue', 'green', 'red', 'cyan', 'magenta', 'yellow', 'grey']
+
+    df_print = dataframe.copy()
+    df_print['count'] = 1
+
+    default_col_size = 10
+    default_row_size = 6
+    if not figure:
+        fig, ax = plt.subplots(nrows=n_rows, ncols=n_cols)
+        fig.set_size_inches(default_col_size*n_cols,default_row_size*n_rows)    
+    else:
+        fig, ax = figure
+
+    for i_tuple, col in enumerate(colums):
+        curr_ax=ax[i_tuple//n_cols][i_tuple%n_cols]
+        
+        # range creation
+        df_grouped = df_print[[col, class_column, 'count']].groupby([col, class_column]).count()
+        col_levels = df_grouped.index.levels[0]
+        target_values = list(set(df_grouped.index.labels[1].tolist()))
+        range_col_value =  [i for i in col_levels.tolist() for _ in range(len(target_values))] 
+        
+        # fill gaps between indexes
+        arrays = [range_col_value, target_values*(len(range_col_value)//len(target_values))]
+        new_index = pd.MultiIndex.from_arrays(arrays, names=(col, class_column))
+        df_grouped = df_grouped.reindex(new_index).fillna(0)
+        
+        # get completed data
+        lines = []
+        for c in range(len(target_values)):
+            l = df_grouped[np.in1d(df_grouped.index.get_level_values(1), [c])]
+            lines.append(l/l.sum())
+        
+        # plotting
+        for i, line in enumerate(lines):
+            line.plot(ax=curr_ax, c=colors[i])            
+        
+        # print areas
+        if plot_area:
+            x = list(range(col_levels.shape[0]))
+            for i, line in enumerate(lines):        
+                y1 = line.values.squeeze()
+                other_lines = []
+                for i2, line2 in enumerate(lines):
+                    if i!=i2:
+                        other_lines.append(line2)
+                lines_matrix = tuple([l.values.squeeze() for l in other_lines])
+                y2 = np.column_stack(lines_matrix).T.max(axis=0)
+
+                #y2 = (c2/c2.sum()).values.squeeze()
+                curr_ax.fill_between(x, y1, y2, where=y1 >= y2, facecolor=colors[i], interpolate=True, alpha=0.3)
+        
+        
+        # other plot things
+        if legend:
+            legend = [f'{class_column}={t_v}' for t_v in target_values]
+            curr_ax.legend(legend)
+
+        curr_ax.set_xlabel(col)
+        curr_ax.set_xticklabels(col_levels)
+    return fig, ax
+
---
+++ examples/example_faced_ratios.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets
+
+import sys
+sys.path.append('..')
+
+from analysis import features
+
+def main():
+    iris = datasets.load_iris()
+    df = pd.DataFrame(data= np.c_[iris['data'], iris['target']],
+                        columns= iris['feature_names'] + ['target'])
+
+    f,a = features.faced_ratios(df, df.columns[:-1].values, 'target')
+    plt.show()
+
+main()
