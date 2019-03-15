83fe7f3307777b1cde2e210da141dc029c0909a4
Ricardo Hortelano <ricardo.hortelano@intelygenz.com>
2019-03-15 14:25:30 +0100

    change to return scalar instead of array

--- analysis/core/core_functions.py
+++ analysis/core/core_functions.py
@@ -4,6 +4,6 @@ def compute_areas_between_lines(y1,y2):
     diff = y1 - y2
     positive_values = np.maximum(diff, 0)
     negative_values = np.minimum(diff, 0)
-    y1_area = np.trapz(positive_values)
-    y2_area = np.trapz(negative_values)
+    y1_area = np.trapz(positive_values)[0]
+    y2_area = np.trapz(negative_values)[0]
     return y1_area, y2_area
